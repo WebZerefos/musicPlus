87c5ea9807f698fde49e701af5b7ac61949f15a4
WebZerefose <anything.codable@gmail.com>
2023-05-05 20:39:03 -0300

    feat: React slider, React track player

---
+++ ios/dummy.swift
@@ -0,0 +1,8 @@
+//
+//  dummy.swift
+//  musicPlus
+//
+//  Created by Victor Zerefos on 05/05/23.
+//
+
+import Foundation
---
+++ src/constants.ts
@@ -0,0 +1,38 @@
+import { Track } from 'react-native-track-player'
+
+export const playListData: Track[] = [
+	{
+		id: 1,
+		title: 'Rains Of Meghalaya - Hanu Dixit',
+		album: 'Youtube',
+		artwork: 'https://i.pinimg.com/280x280_RS/eb/eb/54/ebeb54c06746ed9bccfef8eabaabd805.jpg',
+		url: require('./assets/audio/one.mp3'),
+	},
+	{
+		id: 2,
+		title: 'Shake It - Aakash Gandhi',
+		artist: 'Youtube',
+		album: 'Youtube',
+		artwork:
+			'https://static.wixstatic.com/media/96b5ff_cb486d8139114c128cd550ab6f3eebc5~mv2.jpg/v1/fit/w_320%2Ch_1000%2Cal_c%2Cq_80,enc_auto/file.jpg',
+		url: require('./assets/audio/two.mp3'),
+	},
+	{
+		id: 3,
+		title: 'The Movement of India - Aakash Gandhi',
+		artist: 'Youtube',
+		album: 'Youtube',
+		artwork:
+			'https://images.squarespace-cdn.com/content/v1/60903edb0a3fcd5888c68c43/1a4859b0-2c12-4f7c-88e9-ba20d140cbf6/Where+they+Dwell+1.png?format=500w',
+		url: require('./assets/audio/three.mp3'),
+	},
+	{
+		id: 4,
+		title: 'The Urban Groove - Hanu Dixit',
+		artist: 'Youtube',
+		album: 'Youtube',
+		artwork:
+			'https://images.squarespace-cdn.com/content/v1/60903edb0a3fcd5888c68c43/1cc96afc-3695-423b-a207-958fc32f746a/Eco-david-maxim-micic-album-cover-aria-fawn.png?format=500w',
+		url: require('./assets/audio/four.mp3'),
+	},
+]
